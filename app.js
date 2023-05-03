8e8dab30898c81f2ee1fe3ae2cba8c6fdbaf6613
Riba <andrea.riba@capgemini.com>
2023-05-03 12:29:10 +0200

    one image visualization

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
-const API_KEY = ""
 
-async function fetchData() {
-    const response = await fetch("https://api.openai.com/v1/completions", {
-        method: "POST",
-        headers: {
-            Authorization: `Bearer ${API_KEY}`,
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            model: "davinci",
-            prompt: "Hello, how are you today?",
-            max_tokens: 7
+document.addEventListener('DOMContentLoaded', () => {
+
+    const APILink = "https://api.windy.com/api/webcams/v2"
+
+    let webcamImg = document.getElementById('webcam-img')
+
+    console.log(webcamImg)
+
+    async function getWebcam() {
+
+        let response = await fetch(APILink+"/list/nearby=44.4293,7.19170,200/orderby=distance/limit=1?show=webcams:image,location;", {
+            method: "GET",
+            headers: {
+                "x-windy-key": `${API_KEY}`,
+                "Content-Type": "application/json"
+            }
         })
+
+        let GETData = await response.json()
+
+        const webcamId = GETData["result"]["webcams"][0]['id']
+        const webcamImage = GETData["result"]["webcams"][0]['image']['current']['preview']
+
+        console.log(webcamId, webcamImage)
+
+        return webcamImage
+    }
+
+    getWebcam().then(webcamImage => {
+        webcamImg.src = webcamImage
     })
 
-    const data = await response.json()
-    console.log(data)
-}
+})
 
-fetchData()
